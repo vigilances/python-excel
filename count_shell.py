a93845d93d36cf40f49c4ed0800853e7eb08463c
vigilances <vigilance.zx@gmail.com>
2017-09-19 08:57:24 +0800

    Update count_shell.py
    fix the bug

--- count_shell.py
+++ count_shell.py
@@ -4,7 +4,7 @@ input_month=input("Please input the month:")
 wbk = xlwt.Workbook(encoding='utf-8', style_compression=0)  
 sheet = wbk.add_sheet('sheet 1', cell_overwrite_ok=True)
 sheet_count = wbk.add_sheet('sheet2', cell_overwrite_ok=True)
-book = xlrd.open_workbook("files.xls")
+book = xlrd.open_workbook("file.xls")
 #print("The number of worksheets is {0}".format(book.nsheets))
 #print("Worksheet name(s): {0}".format(book.sheet_names()))
 sh = book.sheet_by_index(0)
@@ -22,19 +22,21 @@ sheet.write(0,2,"记录时间",style)
 sheet.write(0,3,"加班时间",style)
 sheet.write(0,4,"申请调休时间（小时）",style)
 sheet.write(0,5,"累计（小时）",style)
-sheet_count.write_merge(0,1,0,5,"技术一部加班餐补明细",style)
+sheet_count.write_merge(0,1,0,5,"技术一部"+input_month+"月加班餐补明细",style)
 sheet_count.write(2,0,"姓名",style)
+sheet_count.write(2,1,"日期",style)
+sheet_count.write(2,2,"记录时间",style)
 #sheet_count.write_merge(2,2,1,2,input_month+"月餐补天数",style)
-sheet_count.write(2,1,input_month+"月餐补天数",style)
-sheet_count.write(2,2,"每次补助",style)
-sheet_count.write(2,3,"合计补助",style)
-sheet_count.write(2,4,"签字",style)
+#sheet_count.write(2,1,input_month+"月餐补天数",style)
+sheet_count.write(2,3,"每次补助",style)
+sheet_count.write(2,4,"合计补助",style)
+sheet_count.write(2,5,"签字",style)
 i=0
+j=2
 all_time=0
-detail_i=0
 is_overtime={}
 count_money=0
-count_time=0
+
 for rx in range(sh.nrows):  
     id_tag=sh.cell_value(rx,0)
     #第一个必须是打卡号最前面的那个，要不合并单元格会出问题
@@ -57,12 +59,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
                # sheet.write(i,0,name_info)
                 sheet.write_merge(1,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+1,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+1,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+1,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
                 #sheet.write(i,5,all_time)
@@ -71,6 +80,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))      
         all_time=0
+        count_money=0
         print("***********分割线************")
     #谢迪
     elif id_tag=="97":
@@ -92,20 +102,28 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
                # sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
-                sheet.write(i,4,each_time,style)
+                sheet.write(i,4,each_time,style) 
                # sheet.write(i,5,all_time)
                 sheet.write_merge(temp,i,5,5,all_time,style)
                 dates.append(name_info)
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))
         all_time=0
+        count_money=0
         print("***********分割线************")
     #吴仲春
     elif id_tag=="343":
@@ -127,12 +145,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -141,6 +166,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))
         all_time=0
+        count_money=0
         print("***********分割线************")
     #屈喆
     elif id_tag=="371":
@@ -162,12 +188,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5  
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
                # sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,"屈喆",style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,"屈喆",style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
                # sheet.write(i,5,all_time)
@@ -176,6 +209,7 @@ for rx in range(sh.nrows):
                 is_overtime["屈喆"]=str(len(dates))
                 print("datetimes "+str(len(dates)))              
         all_time=0
+        count_money=0
         print("***********分割线************")
     #胡飞
     elif id_tag=="406":
@@ -197,12 +231,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
                # sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
                # sheet.write(i,5,all_time)
@@ -211,6 +252,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))             
         all_time=0
+        count_money=0
         print("***********分割线************")
     #康子庄
     elif id_tag=="418":
@@ -232,12 +274,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -246,6 +295,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))              
         all_time=0
+        count_money=0
         print("***********分割线************")
     #张鑫
     elif id_tag=="22":
@@ -267,12 +317,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
                 #sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
                 #sheet.write(i,5,all_time)
@@ -282,6 +339,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))              
         all_time=0
+        count_money=0
         print("***********分割线************")
     #邓杰
     elif id_tag=="18":
@@ -303,12 +361,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -317,6 +382,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))                
         all_time=0
+        count_money=0
         print("***********分割线************")
     #刘波
     elif id_tag=="26":
@@ -338,12 +404,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -352,6 +425,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))                            
         all_time=0
+        count_money=0
         print("***********分割线************")
     #李俊葶
     elif id_tag=="35":
@@ -373,12 +447,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5 
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -387,6 +468,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))               
         all_time=0
+        count_money=0
         print("***********分割线************")
         #曹然
     elif id_tag=="82":
@@ -408,12 +490,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -422,6 +511,7 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))             
         all_time=0
+        count_money=0
         print("***********分割线************")
     #测试
     elif id_tag=="xx":
@@ -443,12 +533,19 @@ for rx in range(sh.nrows):
                 if(int(time_info[-3:-1])>=30):
                     each_time+=0.5
                 all_time+=each_time
+                count_money+=30
                 date_info=str(sh.cell_value(2,cl))
                 print(date_info)
               #  sheet.write(i,0,name_info)
                 sheet.write_merge(temp,i,0,0,name_info,style) 
                 sheet.write(i,1,(input_month+"."+date_info[-4:-2]),style)
                 sheet.write(i,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write_merge(j+temp,i+2,0,0,name_info,style)
+                sheet_count.write(i+2,1,(input_month+"."+date_info[-4:-2]),style,)
+                sheet_count.write(i+2,2,"18:00-"+time_info[-6:-1],style)
+                sheet_count.write(i+2,3,"30",style)
+                sheet_count.write_merge(j+temp,i+2,4,4,count_money,style)
+                sheet_count.write_merge(j+temp,i+2,5,5,"",style)
                 sheet.write(i,3,each_time,style)
                 sheet.write(i,4,each_time,style)
               #  sheet.write(i,5,all_time)
@@ -457,19 +554,12 @@ for rx in range(sh.nrows):
                 is_overtime[name_info]=str(len(dates))
                 print("datetimes "+str(len(dates)))             
         all_time=0
-        print("***********分割线************")        
-#明细sheet2
-for overtime in is_overtime:
-    detail_i+=1
-    count_money+=30*int(is_overtime[overtime])
-    count_time+=int(is_overtime[overtime])
-    sheet_count.write(2+detail_i,0,overtime,style)
-    sheet_count.write(3+detail_i,0,"合计",style)
-    sheet_count.write(2+detail_i,1,is_overtime[overtime],style)
-    sheet_count.write(2+detail_i,2,30,style)
-    sheet_count.write(2+detail_i,3,30*int(is_overtime[overtime]),style)
-sheet_count.write_merge(3+detail_i,3+detail_i,1,1,count_time,style)
-sheet_count.write_merge(3+detail_i,3+detail_i,2,2,"30",style)
-sheet_count.write_merge(3+detail_i,3+detail_i,3,3,count_money,style)
-
+        count_money=0
+        print("***********分割线************")     
+sheet_count.write(j+temp,0,"合计",style)
+sheet_count.write(j+temp,1,i,style)
+sheet_count.write(j+temp,2,"\\",style)
+sheet_count.write(j+temp,3,"30",style)
+sheet_count.write(j+temp,4,str(i*30),style)
+sheet_count.write(j+temp,5,"\\",style)
 wbk.save("D:\技术一部加班统计---"+input_month+"月.xls")
